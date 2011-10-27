9b1c4e52299af3781bc94034d4b82cd96dfb3238
José Valim <jose.valim@gmail.com>
2011-10-27 18:43:54 +0200

    Generate pointers to ruby directly.

--- register_protocol_handler_wrapper.rb
+++ register_protocol_handler_wrapper.rb
@@ -1,5 +1,12 @@
-require 'erb'
-reg = ERB.new(IO.read("register_protocol_handler_wrapper.reg.erb")).result
+require "rubygems"
+require "erb"
+
+app  = ARGV[0] || "txmt"
+path = ARGV[1] || File.expand_path("..\\ProtocolHandlerWrapper.exe", __FILE__)
+path = path.gsub(/[\\\/]/, "\\\\\\\\")
+ruby = Gem.ruby.gsub(/[\\\/]/, "\\\\\\\\")
+
+reg = ERB.new(IO.read("register_protocol_handler_wrapper.reg.erb")).result(binding)
 reg_path = File.expand_path("../register_protocol_handler_wrapper.reg", __FILE__)
 File.open(reg_path, "w") { |f| f.write(reg) }
-system("cmd /c regedit #{ reg_path }")
+system("cmd /c regedit #{reg_path}")
